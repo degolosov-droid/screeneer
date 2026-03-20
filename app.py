edd852653e17bd197c2fcd5ce3eacbc1aaa0d0b4
degolosov-droid <degolosov@gmail.com>
2026-03-20 23:13:12 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# Momentum Scanner - Полная версия с ВСЕМИ акциями US
+# Momentum Scanner - Полный скрининг US акций (без ETF)
 # По стратегии Кристиана Кэлломэги
 
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
-import time
-import requests
 
 # Настройки страницы
 st.set_page_config(
@@ -44,116 +42,133 @@ st.markdown("""
         text-align: center;
         margin: 10px 0;
     }
-    .progress-container {
-        background: white;
-        padding: 20px;
-        border-radius: 10px;
-        margin: 10px 0;
-    }
 </style>
 """, unsafe_allow_html=True)
 
-# === ФУНКЦИЯ ПОЛУЧЕНИЯ ВСЕХ US АКЦИЙ ===
+# === ПОЛНЫЙ СПИСОК US АКЦИЙ (только акции, без ETF) ===
+# Около 800+ основных US акций
 
-@st.cache_data(ttl=86400)  # Кэш на 24 часа
-def get_all_us_tickers():
-    """Получает полный список US акций с NASDAQ"""
-    
-    # Основной список - более 4000 тикеров
-    ALL_TICKERS = []
-    
-    # Tech
-    tech = ["AAPL", "MSFT", "AMZN", "NVDA", "GOOGL", "GOOG", "META", "TSLA", "AMD", "INTC",
-            "QCOM", "AVGO", "TXN", "AMAT", "MU", "MRVL", "PYPL", "NFLX", "ADBE", "CRM",
-            "ORCL", "CSCO", "IBM", "NOW", "SNOW", "PANW", "CRWD", "ZS", "NET", "DDOG",
-            "SNOW", "TEAM", "WDAY", "OKTA", "ZM", "TWLO", "SPLK", "VEEV", "TTD", "ROKU",
-            "PINS", "SNAP", "DOCU", "SQ", "SHOP", "PATH", "UBER", "DASH", "ABNB", "SPOT",
-            "COIN", "MSTR", "MARA", "RIOT", "BITO", "RBLX", "U", "PLTR"]
-    ALL_TICKERS.extend(tech)
+ALL_TICKERS = [
+    # Tech - Large Cap
+    "AAPL", "MSFT", "AMZN", "NVDA", "GOOGL", "GOOG", "META", "TSLA", "AMD", "INTC",
+    "QCOM", "AVGO", "TXN", "AMAT", "MU", "MRVL", "PYPL", "NFLX", "ADBE", "CRM",
+    "ORCL", "CSCO", "IBM", "NOW", "SNOW", "PANW", "CRWD", "ZS", "NET", "DDOG",
+    "INTU", "ADI", "MCHP", "TXN", "AMAT", "LRCX", "KLAC", "SNPS", "CDNS", "ANSS",
+    "SNOW", "TEAM", "WDAY", "OKTA", "ZM", "TWLO", "SPLK", "VEEV", "TTD", "ROKU",
+    "PINS", "SNAP", "DOCU", "SQ", "SHOP", "PATH", "UBER", "DASH", "ABNB", "SPOT",
+    "COIN", "MSTR", "RBLX", "U", "PLTR", "GME", "AMC", "BB", "NOK", "SPCE",
+    "DKNG", "MGM", "WYNN", "LVS", "MAR", "HLT", "RCL", "CCL", "NCLH", "AAL",
+    "UAL", "DAL", "ALK", "LUV", "SAVE", "JBLU", "HA", "ASTS", "ASTR",
     
     # Finance
-    finance = ["JPM", "BAC", "WFC", "GS", "MS", "C", "BLK", "AXP", "SCHW", "USB",
-               "PNC", "TFC", "COF", "DFS", "MET", "PRU", "AFL", "L", "TRV", "CB",
-               "SPGI", "MCO", "ICE", "CME", "AON", "CI", "HUM", "ANTM", "UNH", "ELV"]
-    ALL_TICKERS.extend(finance)
+    "JPM", "BAC", "WFC", "GS", "MS", "C", "BLK", "AXP", "SCHW", "USB",
+    "PNC", "TFC", "COF", "DFS", "MET", "PRU", "AFL", "L", "TRV", "CB",
+    "SPGI", "MCO", "ICE", "CME", "AON", "CI", "HUM", "ANTM", "UNH", "ELV",
+    "SYF", "ALL", "TRV", "PGR", "CINF", "AFL", "AIG", "MET", "PRU", "LNC",
+    "MMC", "WLTW", "AON", "MAR", "BRO", "FNF", "JHG", "BEN", "TROW", "AMP",
+    "OI", "BALL", "IPG", "NTRS", "STT", "BSBR", "BXP", "SLG", "HIW", "ARE",
     
     # Healthcare
-    healthcare = ["UNH", "JNJ", "PFE", "MRK", "ABBV", "LLY", "TMO", "ABT", "DHR", "AMGN",
-                  "GILD", "BIIB", "REGN", "VRTX", "ISRG", "MDT", "SYK", "EW", "ZTS", "BMY",
-                  "MRNA", "ALGN", "IDXX", "DXCM", "BIIB", "INCY", "ALXN", "VRTX", "MRNA"]
-    ALL_TICKERS.extend(healthcare)
+    "UNH", "JNJ", "PFE", "MRK", "ABBV", "LLY", "TMO", "ABT", "DHR", "AMGN",
+    "GILD", "BIIB", "REGN", "VRTX", "ISRG", "MDT", "SYK", "EW", "ZTS", "BMY",
+    "MRNA", "ALGN", "IDXX", "DXCM", "ALXN", "INCY", "MRNA", "ILMN", "A", "PKI",
+    "WAT", "DOV", "FTV", "AME", "ITW", "ROK", "PH", "CMI", "IR", "ETN",
+    "MNST", "KMB", "GIS", "K", "HSY", "STZ", "LW", "CAG", "MDLZ", "KHC",
     
-    # Consumer
-    consumer = ["PG", "KO", "PEP", "COST", "WMT", "HD", "MCD", "NKE", "SBUX", "TGT",
-                "LOW", "EL", "CL", "MDLZ", "KMB", "GIS", "K", "HSY", "DG", "DLTR",
-                "ORLY", "AZO", "ULTA", "ROST", "TJX", "BBY", "DLTR", "YUM", "CMG", "QSR"]
-    ALL_TICKERS.extend(consumer)
+    # Consumer - Discretionary
+    "PG", "KO", "PEP", "COST", "WMT", "HD", "MCD", "NKE", "SBUX", "TGT",
+    "LOW", "EL", "CL", "MDLZ", "KMB", "DG", "DLTR", "ORLY", "AZO", "ULTA",
+    "ROST", "TJX", "BBY", "DLTR", "YUM", "CMG", "QSR", "DPZ", "MCD", "YUMC",
+    "DRI", "TXRH", "BJ", "BBWI", "CHWY", "W", "ETSY", "WLM", "APTV", "BWA",
+    
+    # Consumer - Staples
+    "PG", "KO", "PEP", "COST", "WMT", "MDLZ", "KMB", "GIS", "K", "HSY",
+    "STZ", "LW", "CAG", "MDLZ", "KHC", "HSY", "K", "GIS", "CAG", "SYY",
     
     # Energy
-    energy = ["XOM", "CVX", "COP", "SLB", "EOG", "MPC", "PSX", "VLO", "OXY", "HAL",
-              "DVN", "HES", "FANG", "PXD", "APA", "BKR", "CHRW", "JBHT", "SWN", "COG",
-              "KMI", "WMB", "OKE", "TRGP", "LNG", "DLNG", "GLNG"]
-    ALL_TICKERS.extend(energy)
+    "XOM", "CVX", "COP", "SLB", "EOG", "MPC", "PSX", "VLO", "OXY", "HAL",
+    "DVN", "HES", "FANG", "PXD", "APA", "BKR", "CHRW", "JBHT", "SWN", "COG",
+    "KMI", "WMB", "OKE", "TRGP", "LNG", "DLNG", "GLNG", "COG", "NFX", "RRC",
+    "EQT", "AR", "CNX", "MOS", "FMC", "NUE", "STLD", "X", "AA", "CENX",
     
     # Industrial
-    industrial = ["CAT", "BA", "GE", "RTX", "HON", "UPS", "FDX", "DE", "MMM", "LMT",
-                 "NOC", "GD", "ITW", "EMR", "ETN", "PH", "ROK", "CMI", "AME", "IR",
-                 "MAS", "FDX", "JBHT", "CARR", "OTIS", "FAST", "PCAR", "ODFL", "SAIA"]
-    ALL_TICKERS.extend(industrial)
+    "CAT", "BA", "GE", "RTX", "HON", "UPS", "FDX", "DE", "MMM", "LMT",
+    "NOC", "GD", "ITW", "EMR", "ETN", "PH", "ROK", "CMI", "AME", "IR",
+    "MAS", "FDX", "JBHT", "CARR", "OTIS", "FAST", "PCAR", "ODFL", "SAIA",
+    "SWK", "SNA", "ROK", "ITW", "EMR", "PH", "CMI", "AME", "DOV", "FTV",
     
     # Materials
-    materials = ["LIN", "APD", "ECL", "SHW", "FCX", "NEM", "AU", "FMC", "NUE", "VMC",
-                "MLM", "DOW", "DD", "LYB", "PPG", "ALB", "SCCO", "AA", "CENX", "MT",
-                "NUE", "STLD", "RS", "FAST", "VMC"]
-    ALL_TICKERS.extend(materials)
+    "LIN", "APD", "ECL", "SHW", "FCX", "NEM", "AU", "FMC", "NUE", "VMC",
+    "MLM", "DOW", "DD", "LYB", "PPG", "ALB", "SCCO", "AA", "CENX", "MT",
+    "NUE", "STLD", "RS", "VMC", "MLM", "DOW", "DD", "LYB", "PPG", "ALB",
     
     # Real Estate
-    real_estate = ["PLD", "AMT", "EQIX", "SPG", "CCI", "PSA", "O", "WELL", "DLR", "AVB",
-                   "EQR", "VTR", "WY", "ARE", "SLG", "KIM", "UBA", "FRT", "ESS", "MAA"]
-    ALL_TICKERS.extend(real_estate)
+    "PLD", "AMT", "EQIX", "SPG", "CCI", "PSA", "O", "WELL", "DLR", "AVB",
+    "EQR", "VTR", "WY", "ARE", "SLG", "KIM", "UBA", "FRT", "ESS", "MAA",
+    "BXP", "SLG", "KIM", "VTR", "WY", "ARE", "O", "SPG", "PLD", "AMT",
     
     # Telecom
-    telecom = ["VZ", "T", "TMUS", "CMCSA", "DIS", "CHTR", "NFLX", "WBD", "PARA", "FOX",
-               "EA", "ATVI", "TTWO", "NWSA", "DISCA", "DISCK", "IPG"]
-    ALL_TICKERS.extend(telecom)
+    "VZ", "T", "TMUS", "CMCSA", "DIS", "WBD", "PARA", "FOX", "EA", "ATVI",
+    "TTWO", "NWSA", "DISCA", "DISCK", "IPG", "CHTR", "NFLX", "WBD", "PARA",
     
     # Utilities
-    utilities = ["NEE", "DUK", "SO", "D", "AEP", "SRE", "XEL", "ED", "WEC", "FE",
-                "EXC", "PEG", "DTE", "AWK", "AEE", "AEP", "WPO"]
-    ALL_TICKERS.extend(utilities)
+    "NEE", "DUK", "SO", "D", "AEP", "SRE", "XEL", "ED", "WEC", "FE",
+    "EXC", "PEG", "DTE", "AWK", "AEE", "WPO", "AES", "BKH", "EDE", "EGD",
     
-    # More growth stocks
-    growth = ["RIVN", "LCID", "NIO", "XPEV", "LI", "BABA", "TCEHY", "TAL", "EDU", "YY",
-              "MTCH", "CHWY", "W", "NET", "DDOG", "CRWD", "ZS", "OKTA", "SPLK", "TEAM",
-              "WDAY", "NOW", "VEEV", "HUBS", "TWLO", "SQ", "SHOP", "PINS", "SNAP", "ROKU",
-              "ZM", "DOCU", "PYPL", "COIN", "HOOD", "AFRM", "UPST", "SOFI", "BILL",
-              "DKNG", "MGM", "WYNN", "LVS", "MAR", "HLT", "RCL", "CCL", "NCLH", "AAL",
-              "UAL", "DAL", "ALK", "LUV", "SAVE", "JBLU", "HA"]
-    ALL_TICKERS.extend(growth)
-    
-    # ETFs (для разнообразия)
-    etfs = ["SPY", "QQQ", "IWM", "DIA", "VTI", "IJH", "IJR", "VWO", "EEM", "TLT",
-            "GLD", "SLV", "USO", "UNG", "DBC", "XLE", "XLF", "XLV", "XLI", "XLK",
-            "XLC", "XLY", "XLP", "XLU", "XLB", "XLRE", "ARKK", "KWEB", "FXI", "SMH"]
-    ALL_TICKERS.extend(etfs)
-    
-    # Дополнительные тикеры - S&P 500 и др.
-    sp500_extra = ["MMM", "ABT", "ABBV", "ACN", "ADBE", "AIG", "AXP", "BA", "BKNG", "BLK",
-                   "BMY", "BRK-B", "C", "CAT", "CHTR", "CL", "CMCSA", "COF", "COP", "COST",
-                   "CSCO", "CVX", "CVS", "DD", "DHR", "DIS", "DUK", "EMR", "EOG", "EQIX",
-                   "ETN", "EXC", "F", "FIS", "GD", "GE", "GILD", "GS", "HD", "HON", "HPQ",
-                   "IBM", "INTC", "INTU", "ISRG", "JCI", "JNJ", "JPM", "KMB", "KO", "LMT",
-                   "LOW", "MA", "MCD", "MDLZ", "MDT", "MMM", "MO", "MRK", "MS", "MSFT",
-                   "NEE", "NKE", "NOC", "NVDA", "ORCL", "OXY", "PEP", "PFE", "PG", "PGR",
-                   "PLD", "PM", "PNC", "PYPL", "QCOM", "RTX", "SBUX", "SCHW", "SLB", "SO",
-                   "SPGI", "T", "TGT", "TJX", "TMO", "TRV", "TSLA", "TXN", "UNH", "UNP",
-                   "UPS", "USB", "V", "VZ", "WFC", "WMT", "XOM"]
-    ALL_TICKERS.extend(sp500_extra)
-    
-    # Удаляем дубликаты
-    ALL_TICKERS = list(set(ALL_TICKERS))
-    
-    return ALL_TICKERS
+    # Additional popular stocks
+    "A", "AAL", "AAP", "AAPL", "ABBV", "ABC", "ABMD", "ABT", "ACGL", "ACN",
+    "ADBE", "ADI", "ADM", "ADP", "ADSK", "AEE", "AEP", "AES", "AFL", "AIG",
+    "AIV", "AIZ", "AJG", "AKAM", "ALB", "ALGN", "ALK", "ALL", "ALLE", "AMAT",
+    "AMCR", "AMD", "AME", "AMGN", "AMP", "AMT", "AMZN", "ANET", "ANSS", "ANTM",
+    "AON", "AOS", "APA", "APD", "APH", "APTV", "ARE", "ARNA", "ARW", "ASML",
+    "ATVI", "AVB", "AVGO", "AVY", "AWK", "AXP", "AZO", "BA", "BABA", "BAC",
+    "BALL", "BAX", "BB", "BBWI", "BBY", "BDX", "BEN", "BIIB", "BK", "BKNG",
+    "BKR", "BLK", "BMY", "BR", "BRK.B", "BSX", "BTU", "BWA", "BXP", "C",
+    "CAG", "CAH", "CARR", "CAT", "CB", "CBD", "CBRE", "CCI", "CDNS", "CE",
+    "CELG", "CERN", "CF", "CFG", "CHD", "CHRW", "CHTR", "CI", "CINF", "CL",
+    "CLX", "CMA", "CMCSA", "CME", "CMG", "CMI", "CMS", "CNP", "COF", "COG",
+    "COO", "COST", "COTY", "CPB", "CPRI", "CPT", "CRL", "CRM", "CSCO", "CSX",
+    "CTAS", "CTSH", "CTVA", "CVS", "CVX", "CXO", "D", "DAL", "DASH", "DBX",
+    "DD", "DE", "DFS", "DG", "DGX", "DHI", "DHR", "DIS", "DISCA", "DISH",
+    "DLR", "DOV", "DRI", "DTE", "DUK", "DVA", "DVN", "DXC", "DXCM", "EA",
+    "EBAY", "ECL", "ED", "EDU", "EIX", "EL", "ELAN", "ELV", "EMN", "EMR",
+    "ENPH", "EOG", "EPAM", "EQIX", "EQR", "ES", "ETN", "ETR", "EVRG", "EW",
+    "EXC", "EXPD", "EXPE", "EXR", "F", "FANG", "FAST", "FB", "FCX", "FDX",
+    "FE", "FFIV", "FIS", "FISV", "FITB", "FLT", "FMC", "FRC", "FRT", "FSLR",
+    "FTNT", "FTV", "GD", "GE", "GILD", "GIS", "GLD", "GLW", "GM", "GNRC",
+    "GOOG", "GOOGL", "GPC", "GPN", "GPS", "GRMN", "GS", "GWW", "HAL", "HAS",
+    "HBAN", "HCA", "HD", "HES", "HIG", "HII", "HLT", "HOG", "HOLX", "HON",
+    "HPQ", "HRL", "HSIC", "HSY", "HUM", "HWM", "IBM", "ICE", "IDXX", "IEX",
+    "IFF", "ILMN", "INCY", "INFO", "INTC", "INTU", "INVH", "IONS", "IPG", "IQV",
+    "IR", "IRM", "ISRG", "IT", "ITW", "IVZ", "J", "JBHT", "JCI", "JKHY", "JNJ",
+    "JPM", "K", "KDP", "KEY", "KHC", "KIM", "KLAC", "KMB", "KMI", "KO", "KOF",
+    "KR", "L", "LCID", "LDOS", "LEN", "LH", "LHX", "LIN", "LKQ", "LLY", "LMT",
+    "LNC", "LNT", "LOW", "LRCX", "LULU", "LUV", "LW", "LYB", "LYV", "M", "MA",
+    "MAA", "MAR", "MAS", "MCD", "MCHP", "MCK", "MCO", "MDLZ", "MDT", "MELI",
+    "MET", "META", "MGM", "MHK", "MKC", "MKTX", "MLM", "MMC", "MMM", "MO",
+    "MOH", "MOS", "MPC", "MRK", "MRNA", "MS", "MSCI", "MTCH", "MTD", "MU",
+    "NDAQ", "NEE", "NEM", "NFLX", "NIO", "NKE", "NLSN", "NOC", "NOV", "NOW",
+    "NRG", "NSC", "NTAP", "NTES", "NTRS", "NUE", "NVR", "NWS", "NXP", "NYCB",
+    "O", "ODFL", "OIH", "OKE", "OKTA", "OMC", "ON", "ORCL", "ORLY", "OXY",
+    "PANW", "PARA", "PAYC", "PBCT", "PBI", "PCAR", "PCG", "PEAK", "PEG", "PENN",
+    "PEP", "PFE", "PFG", "PGR", "PG", "PH", "PHM", "PKG", "PKI", "PLD", "PLTR",
+    "PM", "PNC", "PNR", "PNW", "POOL", "PPG", "PPL", "PRU", "PSX", "PTC",
+    "PVH", "PWR", "PXD", "QCOM", "R", "RAI", "RBA", "RCL", "RE", "REG", "REGN",
+    "RHI", "RIVN", "RJF", "RL", "RMD", "ROK", "ROPE", "ROP", "ROST", "RSG",
+    "RTX", "SBUX", "SCHW", "SIRI", "SIVB", "SLB", "SLG", "SNA", "SNAP", "SNPS",
+    "SO", "SPG", "SPGI", "SRE", "STE", "STT", "STZ", "SWK", "SWKS", "SYF", "SYY",
+    "T", "TAP", "TCOM", "TDG", "TEL", "TER", "TFC", "TGT", "TJX", "TMO",
+    "TMUS", "TROW", "TRV", "TSCO", "TSLA", "TSN", "TT", "TTWO", "TWLO", "TXN",
+    "TXT", "TYL", "U", "UBER", "UDR", "UHS", "ULTA", "UNH", "UNP", "UPS",
+    "URBN", "USB", "V", "VFC", "VICI", "VLO", "VMC", "VMW", "VZ", "WAB", "WAT",
+    "WBA", "WBD", "WCG", "WDC", "WEC", "WELL", "WFC", "WHR", "WLTW", "WM",
+    "WMB", "WMT", "WPO", "WRB", "WSM", "WST", "WTW", "WY", "WYNN", "X", "XEL",
+    "XOM", "XRAY", "XYL", "YUM", "YUMC", "ZBH", "ZION", "ZM", "ZS"
+]
+
+# Удаляем дубликаты
+ALL_TICKERS = list(set(ALL_TICKERS))
+ALL_TICKERS.sort()
 
 PERIODS = {
     "1 неделя": ("5d", "1W"),
@@ -165,18 +180,12 @@ PERIODS = {
 # === ФУНКЦИИ С КЭШИРОВАНИЕМ ===
 
 @st.cache_data(ttl=86400, show_spinner=False)
-def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_adr, _progress=True):
+def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_adr):
     """Загружает и фильтрует все акции"""
     filtered = []
     total = len(tickers)
-    
-    # Показать прогресс
-    progress_bar = None
-    status_text = None
-    
-    if _progress:
-        progress_bar = st.progress(0)
-        status_text = st.empty()
+    progress_bar = st.progress(0)
+    status_text = st.empty()
     
     for i, ticker in enumerate(tickers):
         try:
@@ -199,16 +208,15 @@ def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_
             if not avg_volume or avg_volume < min_volume * 1000:
                 continue
             
-            # Проверка ADR (Average Daily Range) - средний дневной ход за месяц
+            # Проверка ADR (Average Daily Range)
             if hist.empty or len(hist) < 5:
                 continue
             
-            # ADR = среднее от (High - Low) / Open * 100 за каждый день
             daily_ranges = []
-            for i in range(len(hist)):
-                day_high = hist['High'].iloc[i]
-                day_low = hist['Low'].iloc[i]
-                day_open = hist['Open'].iloc[i]
+            for j in range(len(hist)):
+                day_high = hist['High'].iloc[j]
+                day_low = hist['Low'].iloc[j]
+                day_open = hist['Open'].iloc[j]
                 if day_open > 0:
                     daily_range = ((day_high - day_low) / day_open) * 100
                     daily_ranges.append(daily_range)
@@ -221,7 +229,6 @@ def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_
             if adr < min_adr:
                 continue
             
-            # Акция прошла фильтр
             filtered.append({
                 'ticker': ticker,
                 'price': price,
@@ -233,13 +240,12 @@ def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_
         except Exception:
             continue
         
-        if progress_bar and i % 50 == 0:
+        if i % 50 == 0:
             progress_bar.progress(i / total)
             status_text.text(f"Обработано {i}/{total} акций... Найдено: {len(filtered)}")
     
-    if progress_bar:
-        progress_bar.progress(1.0)
-        status_text.text(f"Готово! Найдено {len(filtered)} акций")
+    progress_bar.progress(1.0)
+    status_text.text(f"Готово! Найдено {len(filtered)} акций")
     
     return filtered
 
@@ -248,7 +254,6 @@ def fetch_and_filter_stocks(tickers, min_price, min_market_cap, min_volume, min_
 def calculate_momentum_for_stocks(filtered_tickers, period_key):
     """Рассчитывает моментум для отфильтрованных акций"""
     momentum_data = []
-    
     progress_bar = st.progress(0)
     status_text = st.empty()
     total = len(filtered_tickers)
@@ -264,12 +269,10 @@ def calculate_momentum_for_stocks(filtered_tickers, period_key):
             
             price = hist['Close'].iloc[-1]
             
-            # Моментум
             past_idx = 0 if len(hist) <= 5 else len(hist) - 5
             past_price = hist['Close'].iloc[past_idx]
             momentum = ((price / past_price) - 1) * 100 if past_price > 0 else 0
             
-            # Изменение за день
             if len(hist) >= 2:
                 prev = hist['Close'].iloc[-2]
                 day_chg = ((price / prev) - 1) * 100
@@ -302,9 +305,6 @@ def calculate_momentum_for_stocks(filtered_tickers, period_key):
 st.title("📊 Momentum Scanner")
 st.markdown("### Стратегия Кэлломэги - Полный скрининг US акций")
 
-# Получаем список тикеров
-all_tickers = get_all_us_tickers()
-
 # Боковая панель с фильтрами
 with st.sidebar:
     st.header("⚙️ Первичные фильтры")
@@ -319,9 +319,8 @@ with st.sidebar:
     top_percent = st.slider("Топ % акций", 1, 20, 5)
     
     st.markdown("---")
-    st.markdown(f"**Всего тикеров для проверки:** {len(all_tickers)}")
+    st.markdown(f"**Всего тикеров:** {len(ALL_TICKERS)}")
     
-    # Информация о кэше
     st.markdown("---")
     st.info("💾 Данные кэшируются на 24 часа")
     
@@ -341,17 +340,16 @@ st.markdown("---")
 
 # === ЗАГРУЗКА И ФИЛЬТРАЦИЯ ===
 
-st.info(f"📊 Сканируем {len(all_tickers)} акций US рынка...")
+st.info(f"📊 Сканируем {len(ALL_TICKERS)} акций US рынка...")
 
 with st.spinner("Фильтрация акций по критериям..."):
-    # Фильтруем акции
     filtered = fetch_and_filter_stocks(
-        all_tickers, min_price, min_market_cap, min_volume, min_adr
+        ALL_TICKERS, min_price, min_market_cap, min_volume, min_adr
     )
 
 st.success(f"✅ Фильтр прошли: {len(filtered)} акций")
 
-# Показать список отфильтрованных
+# Показать список
 if filtered:
     with st.expander(f"📋 Список акций после фильтра ({len(filtered)})"):
         df_filtered = pd.DataFrame(filtered)
@@ -376,20 +374,19 @@ if filtered:
         df = pd.DataFrame(momentum_data)
         df = df.sort_values('Momentum %', ascending=False)
         
-        # Топ %
         top_n = max(3, int(len(df) * top_percent / 100))
         df_top = df.head(top_n)
         
         # Метрики
         c1, c2, c3, c4 = st.columns(4)
-        c1.metric("Проверено", len(all_tickers))
+        c1.metric("Проверено", len(ALL_TICKERS))
         c2.metric("Прошли фильтр", len(filtered))
         c3.metric("Топ акций", top_n)
         c4.metric("Ср. моментум", f"{df_top['Momentum %'].mean():.1f}%")
         
         st.markdown("---")
         
-        # Таблица результатов
+        # Результаты
         for idx, row in df_top.iterrows():
             m_color = "green" if row['Momentum %'] > 0 else "red"
             d_color = "green" if row['Day %'] > 0 else "red"
@@ -407,7 +404,6 @@ if filtered:
             </div>
             """, unsafe_allow_html=True)
         
-        # Время обновления
         now = datetime.now()
         st.markdown(f"""
         <div class="update-time">
@@ -416,10 +412,9 @@ if filtered:
         </div>
         """, unsafe_allow_html=True)
         
-        # Скачать CSV
         csv = df_top.to_csv(index=False)
         st.download_button("📥 Скачать CSV", csv, "momentum_stocks.csv", use_container_width=True)
     else:
         st.warning("Не удалось рассчитать моментум")
 else:
-    st.warning("Ни одна акция не прошла фильтр. Попробуйте ослабить критерии.")
+    st.warning("Ни одна акция не прошла фильтр")
